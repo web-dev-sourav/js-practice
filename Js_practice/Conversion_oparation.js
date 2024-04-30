d6f770ee8673f9189f26814da4939d81c06dc7e5
web-dev-sourav <166805073+web-dev-sourav@users.noreply.github.com>
2024-04-30 23:34:15 +0000

    Operation

---
+++ Js_practice/Conversion_oparation.js
@@ -0,0 +1,28 @@
+
+
+// "50"  => 50
+// "50abc" => NaN (Not a number)
+//true = 1, false = 0
+//Empty String = false
+//String with value = true
+
+
+
+// console.log(typeof(turnBoolean));
+// console.log(turnBoolean);
+// console.log(signIn);
+
+// let signIn2 = "0"
+// let makeItB = Boolean(signIn)
+
+// console.log(typeof(signIn));
+// console.log(makeItB);
+// console.log(typeof(makeItB));
+
+
+//-------------------------------------------Operation------------------------------------------------//
+
+let x = 4
+let y = x++
+
+console.log(x , y);

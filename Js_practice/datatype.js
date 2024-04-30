42f622fab848e8c105cf74dcb90353f8b7e271c0
web-dev-sourav <166805073+web-dev-sourav@users.noreply.github.com>
2024-04-30 22:21:04 +0000

    data type

---
+++ Js_practice/datatype.js
@@ -0,0 +1,9 @@
+"Use Strict"; // Treat all Js as Newer version Of JS
+// alert( 3+3 ) we are using node.js not browser.
+// Number = 2 to power 53
+//bigint
+//string = ""
+//boolean= True/False
+// null= standalone Value
+//Undefine = the value is not defined
+//symbol = Unique 
---
+++ Js_practice/variable.js
@@ -0,0 +1,13 @@
+const price = "2000"
+let name = "Sourav"
+var Batch = "101"
+
+name = "Soumik"
+Batch = "102" 
+
+name = "Arjon"
+Batch = "103"
+console.table([price , name , Batch]);
+
+// // Let is mostly used to store data. Var also be used . Let/Var data can be changed . And Cont is also use
+// to store data but const data can't be changed

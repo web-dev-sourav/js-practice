c4735165d7803a6f24dbc685a83280f8267a40d6
Sourav Kumar Shill <166805073+web-dev-sourav@users.noreply.github.com>
2024-05-01 22:45:47 +0000

    new change

---
+++ Js_practice/data_type_Summ.js
@@ -0,0 +1,29 @@
+// data type is two type accourding how we store those data.
+
+//Primitive: 7 type:
+//String , Number , Boolean , null , undefined , symbol , BigInt .
+
+// Reference Type/ NonPrimitive
+//type : Array , Objects , Function.
+
+                           //Premitive Datatypes
+                           //-------------------\\
+
+// Type                                   typeof
+
+// i)             Number                               number
+// ii)            String                                   string
+// iii)           Boolean                               boolean
+// iv)           Bigint                                   bigint
+// v)            Symbol                                symbol
+// vi)           Null                                      object
+// vii)          Undefined                           undefined
+
+
+// * Non-Premitive OR Referance OR Object datatype
+
+//                   Type                                   typeof
+
+// i)               Object                                object
+// ii)              Array                                  object
+// iii)             Function                            function(object)

ef92aa4cea6b9fc7d5b35e989ab4e56c7c9fc8e8
Sourav Kumar Shill <166805073+web-dev-sourav@users.noreply.github.com>
2024-05-02 23:32:33 +0000

    change 2

---
+++ Js_practice/num.js
@@ -0,0 +1,27 @@
+const roll = 200
+const rollNo = new Number (202)
+
+// console.log(roll);
+// console.log(rollNo);
+
+// console.log(roll.toString());
+
+// console.log(typeof(roll));
+
+const Lakhs = 100000000000000
+
+// console.log(Lakhs.toLocaleString('en-IN'));
+
+
+// ---------------------------------------Maths-------------------------------------------------------
+// console.log(Math.round(5.5));
+
+//math.random give us value between 0-1
+
+// console.log(Math.random());
+// console.log((Math.random(3)*10)+100);
+
+const max = 10
+const min = 5
+
+console.log(Math.floor(Math.random() * max-min + 1) + min);

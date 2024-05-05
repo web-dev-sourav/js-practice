89e48534a51ad955cbb23e9b3de6e8b88b968db2
Sourav Kumar Shill <166805073+web-dev-sourav@users.noreply.github.com>
2024-05-05 21:53:37 +0000

    array

---
+++ js-02/array.js
@@ -0,0 +1,24 @@
+//array
+let array = [0,1,2,3,4,5,6,"ami"]
+// console.log(array[0]);
+// console.log(3);
+
+// let newarray = new Array("sourav")
+//  console.log(newarray);
+
+//Array Methods
+
+array.push(7,8,9,10)
+// console.log(array);
+
+// array.unshift(90)
+// array.shift()
+// console.log(array);
+
+// console.log(array.includes(6));
+// console.log(array.indexOf("ami"));
+
+let newarr = array.join()
+// console.log(newarr);
+// console.log(typeof newarr);
+// console.log(typeof array);
---
+++ js-02/array02.js
@@ -0,0 +1,38 @@
+const new1 = [ "a", "b", "c" ,"d"]
+const new2 = ['1' ,'2', '3']
+
+new1.push(new2)
+
+// console.log(Arrayprb);
+
+// var test1 = new1.length
+// console.log(test1);
+
+// // console.log(new1.length());
+
+// var test = new1.concat(new2)
+// console.log(test);
+
+//sprade oratator
+
+const allTestmain = [...new1, ...new2 ]
+// console.log(allTestmain);
+// const allTest = [...new1, ...new2 , ...Arrayprb]
+// console.log(allTest);
+
+const multiarray = [1,2,3,4,[88,66,747],5,6,[55,22,99,[333,666,444,555,222]]]
+
+const multiarray2 = multiarray.flat(Infinity)
+
+// console.log(multiarray2);
+
+// console.log(Array.isArray([ "a", "b", "c" ,"d"]));
+
+// console.log(Array.from(typeof "Sourav"));
+
+
+let num = 102;
+let num1 = 103;
+let num2 = 104;
+
+console.log(Array.of(num ,num1 , num2));
